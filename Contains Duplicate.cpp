83dc202da0013ed7a7ca5c867ca6080d37366ec7
Vaishnavi Ghadge <146657079+vaishnavi902@users.noreply.github.com>
2026-09-09 18:17:18 +0530

    Create Contains Duplicate.cpp

---
+++ Contains Duplicate.cpp
@@ -0,0 +1,26 @@
+/*nums = [1, 2, 3, 1]
+Output = true   else    false */
+
+//OPTIMAL SOLUTION : UNORDERED SET
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+        unordered_set<int> seen;
+
+        for (int num : nums) {
+
+            if (seen.find(num) != seen.end()) {
+                return true;
+            }
+
+            seen.insert(num);
+        }
+
+        return false;
+    }
+};
